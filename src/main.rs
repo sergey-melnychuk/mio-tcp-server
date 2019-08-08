d805e4714c5bc31060a16adb1b60f5e69bea0c3e
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2019-08-08 09:16:49 +0200

    Cleanup braces in `is_double_crnl`

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,11 @@ hello
 ";
 
 fn is_double_crnl(window: &[u8]) -> bool {
-    window.len() == 4 && {
+    window.len() >= 4 &&
         (window[0] == '\r' as u8) &&
-            (window[1] == '\n' as u8) &&
-            (window[2] == '\r' as u8) &&
-            (window[3] == '\n' as u8)
-    }
+        (window[1] == '\n' as u8) &&
+        (window[2] == '\r' as u8) &&
+        (window[3] == '\n' as u8)
 }
 
 fn main() {
